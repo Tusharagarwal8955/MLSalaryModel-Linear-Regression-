739e43a3fcfcb7f9bc634929c1a11e083132a564
Tushar Agrawal <agrawal.edu800@gmail.com>
2021-05-27 23:47:39 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,5 +1,5 @@
 import joblib
 mind = joblib.load("salaryPred.pk1")
-inpt = float(input("How Much Experiece do you have .. ?"))
+inpt = float(input("How Much Experiece do you have .. ?\n\n"))
 result = mind.predict([[inpt]])
-print(result)
+print("Your estimated Salary will be = ", result[0],end ="/n/n")
